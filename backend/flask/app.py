40f93ebd369cfd0cece3992a77ea940bc6a299dd
Alexandra <willia@usi.ch>
2022-10-13 17:00:36 +0200

    task3 done

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,22 +1,14 @@
-from flask import Flask, send_from_directory
-
+from flask import Flask
+from routes.frontend.frontend import home_page, artist_page, album_page, search_page, track_page, static_page
 # ** __name__ -> name of the current Python module -> variable __name__ is used to tell where application is located. This is required to set some paths used by Flask applications. Here init app -> pass __name__ top id app and know in which package to look for resources
 app = Flask(__name__)
 
-# ** use the route() decorator to tell Flask what URL should trigger our 'hello_world' function.
-@app.route("/")
-# In the browser: the default content type is HTML, so HTML in the string will be rendered by the browser.
-def hello_world():
-    return "<p>Hello, World!</p>"
-    # return 'The value of __name__ is {}'.format(__name__)
-
-# ** You can add variable sections to a URL by marking sections with <variable_name>. 
-# ** Your function then receives the <variable_name> as a keyword argument. 
-# Optionally, you can use a converter to specify the type of the argument like <converter:variable_name>.
-# converter types: string (with no /), int, float, path, uuid
-@app.route("/static/<path:path>")
-def static_dir(path):
-    return send_from_directory("static", path)
+app.register_blueprint(home_page)
+app.register_blueprint(artist_page)
+app.register_blueprint(album_page)
+app.register_blueprint(search_page)
+app.register_blueprint(track_page)
+app.register_blueprint(static_page)
     
     
     
