0829deb433df32dc798b8f5b00c99817128d46d0
Alexandra <willia@usi.ch>
2022-11-22 09:57:39 +0100

    album interface works

--- backend/flask/test.py
+++ backend/flask/test.py
@@ -1,10 +1,12 @@
-from interfaces import track
+from interfaces import album
 from database.database import engine
 from sqlalchemy.orm import Session
 
 with Session(engine) as session:
-    print(len(track.get_top_n(3, session)))
+    print(len(album.get_top_n(3, session)))
     print()
-    print(len(track.get_all(session)))
+    print(len(album.get_all(session)))
     print()
-    print(track.get_match("ill", session))
+    print(album.get_match("ill", session))
+    print()
+    print(album.get_tracks(1, session))
