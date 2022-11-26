248d916ce0b81c1ce94abd3af6fa047058702212
Alexandra <willia@usi.ch>
2022-11-26 14:25:21 +0100

    replaced content of route /test with crated interface

--- backend/flask/test/test.py
+++ backend/flask/test/test.py
@@ -3,6 +3,9 @@ from models.track import Track
 from database.database import engine
 from sqlalchemy.orm import Session
 from utils import row2dict
+from interfaces.track import get_top_n
+from typing import List
+from schemas.track import TrackSchemaFull
 
 
 test = Blueprint('test', __name__)
@@ -10,11 +13,6 @@ test = Blueprint('test', __name__)
 @test.route("/test")
 def return_first_db_track() -> Track:
     with Session(engine) as session:
-        # These two are equivalent: latter one is better (safe)
-        # track = session.execute(select(Track)).first()
-        # *! TODO: replace with top_n in interface track.py, then return track.dict()
-        track = session.query(Track).first()
-        print(track.__dict__)
-    return row2dict(track)
+        track: List[TrackSchemaFull]  = get_top_n(1, session)
+    return track[0].dict()
     
-# *! TODOL change session to db
