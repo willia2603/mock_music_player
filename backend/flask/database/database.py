76ec34f0b0ecadb7680f3511d90e811e98aa1939
Alexandra <willia@usi.ch>
2022-11-26 14:21:02 +0100

    changed db entries, re run alembic

--- backend/flask/database/database.py
+++ backend/flask/database/database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DB_URI = 'sqlite:///database/sqlite.db'
 
-# creates db after calling connect() if db doesn't exist. echo = True for logging (goes to stdout) (add , echo=True)
 engine = create_engine(SQLALCHEMY_DB_URI)
 
-# BASE: defines structure of db -> allows to extend (add) model schemas (classes)
 Base = declarative_base()
 
-# class defined nronalmente. instantiate tabelle con alembic -> allows for db mirgation i.e cosi che se cambio la struttura del db, mi converte gia lui i dati e posso fare le query normalmente (usa base e crea la migration in base al BAse)
--- backend/flask/database/seed.py
+++ backend/flask/database/seed.py
@@ -6,12 +6,15 @@ from database.database import engine
 import datetime
 from sqlalchemy.orm import Session
 
-'''
-This file's purpose is to create a set of model instances and add them to the corresponding tables
-'''
 
+# This file's purpose is to empty the db, create a set of model instances and add them to the corresponding tables
 
 def delete_all(session):
+    """
+    Deletes all the rows in all the tables
+
+    :param session: the SQLAlchemy session object that we create in main
+    """
     session.query(Album).delete()
     session.query(Artist).delete()
     session.query(Track).delete()
@@ -20,75 +23,84 @@ def delete_all(session):
 
 
 def populate_db(session):
+    """
+    It creates a bunch of objects and adds them to the database
+
+    :param session: the SQLAlchemy session object that we created in the previous step
+    """
     track1 = Track(
-        name=f"OK Overture",
-        duration=f"22",
-        file=f"./static/music/01. OK Overture.mp3",
+        name="OK Overture",
+        duration=22,
+        file="./static/music/01. OK Overture.mp3",
         popularity=2
     )
     track2 = Track(
-        name=f"02. Bummerland",
-        duration=f"22",
-        file=f"./static/music/02. Bummerland.mp3",
+        name="02. Bummerland",
+        duration=22,
+        file="./static/music/02. Bummerland.mp3",
         popularity=1
     )
 
     # Muse tracks
     track3 = Track(
-        name=f"Will Of The People",
-        duration=f"22",
-        file=f"./static/music/01. Will Of The People.mp3",
+        name="Will Of The People",
+        duration=22,
+        file="./static/music/01. Will Of The People.mp3",
         popularity=1
     )
     track4 = Track(
-        name=f"Won't Stand Down",
-        duration=f"22",
-        file=f"./static/music/04. Won't Stand Down.mp3",
+        name="Won't Stand Down",
+        duration=22,
+        file="./static/music/04. Won't Stand Down.mp3",
         popularity=2
     )
 
     # Muse and AJR tracks
     track5 = Track(
-        name=f"AJR-Muse1",
-        duration=f"22",
-        file=f"./BAH1",
+        name="AJR-Muse 1 Track",
+        duration=22,
+        file="./static/music/AJR-Muse1.txt",
         popularity=1
     )
+    
     track6 = Track(
-        name=f"AJR-Muse2",
-        duration=f"32",
-        file=f"./BAH2",
+        name="AJR-Muse 2 Track",
+        duration=32,
+        file="./static/music/AJR-Muse2.txt",
         popularity=2
     )
     # AJR and Muse album
     album1 = Album(
-        name=f"AJR and Muse",
-        year=int(f"2022"),
+        name="AJR and Muse Album 1",
+        year=2021,
         popularity=2,
+        img_cover="./static/images/ajr_muse_cover1.jpeg",
         tracks=[track5, track6],
     )
 
     # AJR album
     album2 = Album(
-        name=f"OK Orchestra",
-        year=int(f"2021"),
+        name="OK Orchestra",
+        year=2021,
         popularity=1,
+        img_cover="./static/images/ajr_cover.jpeg",
         tracks=[track1, track2],
 
 
     )
     # Muse album
     album3 = Album(
-        name=f"Will Of The People",
-        year=int(f"2022"),
+        name="Will Of The People",
+        year=2021,
         popularity=1,
+        img_cover="./static/images/muse_cover.jpeg",
         tracks=[track3, track4]
     )
 
     # AJR
     artist1 = Artist(
-        name=f"AJR",
-        surname=f"Artist Surname",
+        name="AJR",
+        surname="Artist Surname",
         birthDate=datetime.date.today(),
         popularity=1,
         albums=[album1, album2],
@@ -97,46 +109,84 @@ def populate_db(session):
 
     # Muse
     artist2 = Artist(
-        name=f"Muse",
-        surname=f"Muse",
+        name="Muse",
+        surname="Muse",
         birthDate=datetime.date.today(),
         popularity=2,
         albums=[album1, album3],
-        tracks=[track3, track4, track5, track6]
+        # tracks=[track3, track4, track5, track6]
     )
-
+    
+    # Muse album 
     album4 = Album(
-        name=f"Origin Of Symmetry",
-        year=int(f"2016"),
+        name="Origin Of Symmetry",
+        year=2016,
         popularity=1,
+        img_cover="./static/images/album_placeholder.jpeg",
         artists=[artist2]
     )
-
-    track7 = Track(
-        name=f"AJR-Muse2",
-        duration=f"56",
-        file=f"./BAH3",
-        popularity=1,
-        artists=[artist1, artist2]
-    )
-
+    
+    # AJR Muse ALbum 2
     album5 = Album(
-        name=f"AJR-MUSE-ALBUM",
-        year=int(f"2016"),
-        popularity=1,
+        name="AJR and Muse Album 2",
+        year=2016,
+        popularity=3,
+        img_cover="./static/images/album_placeholder.jpeg",
         artists=[artist1, artist2]
     )
-
-    album6 = Album(
-        name=f"AJR-MUSE-ALBUM-NOARTIST-TRACKWITHARTIST",
-        year=int(f"2016"),
-        popularity=1,
-        tracks=[track7]
+    
+    #  The Score artist
+    artist3 = Artist(
+        name="The Score",
+        surname="The Score",
+        birthDate=datetime.date.today(),
+        popularity=2,
+        # tracks=[track8, track9]
     )
+    
+    # The Score Track
+    track8 = Track(
+        name="08 - Revolution.mp3",
+        duration=33,
+        file="./static/music/08 - Revolution.mp3",
+        popularity=1,
+        # albums=[album6],
+        artists= [artist3]
+    )
+    
+    # The Score album
+    album6 = Album(
+        name="ATLAS",
+        year=2017,
+        popularity=1,
+        img_cover="./static/images/thescore_cover.jpeg",
+        tracks=[track8],
+        artists=[artist3]
+    )
+    
+     # The Score track
+    track9 = Track(
+        name="06 - Unstoppable.mp3",
+        duration=12,
+        file= "./static/music/06 - Unstoppable.mp3",
+        popularity=1,
+        album=album6   
+    )
+    
+    track10 = Track(
+        name="02 - Legend.mp3",
+        duration=12,
+        file= "./static/music/06 - 02 - Legend.mp3",
+        popularity=1,
+        album=album6,
+        artists=[artist3]
+        
+    )
+
     session.add_all(
-        [artist1, artist2,
+        [artist1, artist2, artist3,
          album1, album2, album3, album4, album5, album6,
-         track1, track2, track3, track4, track5, track6])
+         track1, track2, track3, track4, track5, track6, track8, track9, track10])
 
 
 with Session(engine) as session:
