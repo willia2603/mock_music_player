5e10bc3b9856901521dae5c55ed1c640f238464f
Alexandra <willia@usi.ch>
2022-11-28 16:21:39 +0100

    tracks done (implement in future display of artists)

--- backend/flask/blueprints/tracks/routes.py
+++ backend/flask/blueprints/tracks/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, abort
 from jinja2.exceptions import TemplateNotFound
+import interfaces.track as track
+from sqlalchemy.orm import Session
+from database.database import engine
 
 track_page = Blueprint('tracks_page', __name__,
                        template_folder='./templates/tracks')
@@ -7,7 +10,13 @@ track_page = Blueprint('tracks_page', __name__,
 @track_page.route('/tracks')
 def tracks():
     try:
-        return render_template('tracks.html')
+        with Session(engine) as session:
+            tracks = track.get_all(session)
+            context = {
+                'title': 'Tracks',
+                'tracks': tracks
+            }
+            return render_template('tracks.html', **context)
     except TemplateNotFound:
         print('error')
         abort(404)
