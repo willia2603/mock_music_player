ec1f8965967da0ca8e66cedc00010cad084e4142
Alexandra <willia@usi.ch>
2022-11-28 20:14:12 +0100

    task 1 done

--- backend/flask/blueprints/artists/routes.py
+++ backend/flask/blueprints/artists/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, abort
 from jinja2.exceptions import TemplateNotFound
+from sqlalchemy.orm import Session
+from database.database import engine
+import interfaces.artist as artist
 
 artist_page = Blueprint('artists_page', __name__,
                         template_folder='./templates/artists')
@@ -8,7 +11,13 @@ artist_page = Blueprint('artists_page', __name__,
 @artist_page.route('/artist')
 def artists():
     try:
-        return render_template('artists.html')
+        with Session(engine) as session:
+            artists = artist.get_all(session)
+            context = {
+                'title': 'Artists',
+                'artists': artists
+            }
+            return render_template('artists.html', **context)
     except TemplateNotFound:
         print('error')
         abort(404)
--- backend/flask/blueprints/homepage/routes.py
+++ backend/flask/blueprints/homepage/routes.py
@@ -1,12 +1,26 @@
 from flask import Blueprint, render_template, abort
 from jinja2.exceptions import TemplateNotFound
+from database.database import engine
+from sqlalchemy.orm import Session
+import interfaces.album as album
+import interfaces.artist as artist
 
 home_page = Blueprint('homepage_page', __name__, template_folder='./templates/homepage')
 
 @home_page.route('/')
 def homepage():
     try:
-        return render_template('homepage.html')
+        with Session(engine) as session:
+            albums = album.get_top_n(3, session)
+            artists = artist.get_top_n(3, session)
+            context = {
+                'title': 'homepage',
+                'size': len(albums),
+                'albums': albums,
+                'artists': artists
+            }
+            print(len(artists))
+            return render_template('homepage.html', **context)
     except TemplateNotFound:
         print('error')
         abort(404)
